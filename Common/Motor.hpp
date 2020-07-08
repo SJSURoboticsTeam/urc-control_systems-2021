d02e7d3165d19a3ec702503f44fcbb9f779516f2
Andrew E <32277829+AndrewDEmerson@users.noreply.github.com>
2020-07-08 11:22:30 -0700

    Made requested changes to motor interface. SJSURoboticsTeam/urc-central-2021#26

---
+++ Common/Motor.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace sjsu
+{
+/// Abstract interface for devices that drive basic rotational motors.
+/// including motors that that rotate continually such as brushed and brushless DC/AC
+/// motors and excluding stepper and servo motors.
+
+class Motor
+{
+  public:
+  /// Initialize peripherals required to communicate with motor controller. Must be the
+  /// first method called on this driver. After this is called, `Enable()` can
+  /// be called.
+  virtual bool Initilize() = 0;
+  /// This method must be called before running `Speed()`. This function will
+  /// configure the motor controller settings as defined through the constructor of this
+  /// interface's implementation. Some implementations have more detail or
+  /// settings than others.
+  virtual bool Enable() = 0;
+  /// This method makes the motor turn at a given percentage of its maximum speed
+  /// in the direction dictated by the sign of the percent. percentage ranges from
+  /// -1.0f to 1.0f and will clamp the values if greater than this range.
+  void Speed(float percentage);
+};
+} // namespace sjsu
