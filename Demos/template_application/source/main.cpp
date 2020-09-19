7294b17aca377133fc0ba62132ccb53e1d7ed8ea
naterpotatoers <nate.garza@hotmail.com>
2020-09-19 12:03:13 -0700

    removes servo task and replaced w/ onboard led task

--- Demos/template_application/source/main.cpp
+++ Demos/template_application/source/main.cpp
@@ -1,53 +1,36 @@
 #include "L1_Peripheral/lpc40xx/pwm.hpp"
 #include "L3_Application/task_scheduler.hpp"
-#include "L2_HAL/actuators/servo/servo.hpp"
+#include "L2_HAL/boards/sjtwo.hpp"
 #include "utility/log.hpp"
 #include "utility/rtos.hpp"
 
-class ServoTask final : public sjsu::rtos::Task<1024>
+class LEDTask final : public sjsu::rtos::Task<1024>
 {
- public:
-  ServoTask(const char * task_name, sjsu::lpc40xx::Pwm pwmPin)
-      : Task(task_name, sjsu::rtos::Priority::kMedium),
-        servo(pwmPin),
-        changeServoDirection(false)
-  {
-  }
+public:
+  LEDTask(const char *task_name) : Task(task_name, sjsu::rtos::Priority::kMedium) {}
 
   bool Setup() override
   {
-    servo.Initialize();
-    servo.SetPulseBounds(500us, 2500us);
-    servo.SetAngleBounds(0_deg, 180_deg);
-    sjsu::LogInfo("Servo setup complete.");
+    sjtwo::led0.SetAsOutput();
+    sjtwo::led1.SetAsOutput();
+
+    sjtwo::led0.SetHigh();
+    sjtwo::led1.SetLow();
     return true;
   }
 
   bool Run() override
   {
-    if (changeServoDirection)
-    {
-      servo.SetAngle(10_deg);
-      sjsu::LogInfo("Servo set to new angle: 10_deg");
-    }
-    else
-    {
-      servo.SetAngle(170_deg);
-      sjsu::LogInfo("Servo set to new angle: 170_deg");
-    }
-    changeServoDirection = !changeServoDirection;
+    sjtwo::led0.Toggle();
+    sjtwo::led1.Toggle();
     return true;
   }
-
- private:
-  sjsu::Servo servo;
-  bool changeServoDirection;
 };
 
 class PrinterTask final : public sjsu::rtos::Task<1024>
 {
- public:
-  PrinterTask(const char * task_name, const char * message)
+public:
+  PrinterTask(const char *task_name, const char *message)
       : Task(task_name, sjsu::rtos::Priority::kMedium),
         message_(message),
         run_count_(0)
@@ -88,19 +71,18 @@ class PrinterTask final : public sjsu::rtos::Task<1024>
     return true;
   }
 
- private:
+private:
   inline static SemaphoreHandle_t print_mutex;
 
-  const char * message_;
+  const char *message_;
   uint32_t run_count_;
 };
 
 sjsu::rtos::TaskScheduler scheduler;
 PrinterTask printer_one("Printer A", "Printer A: ");
 PrinterTask printer_two("Printer B", "Printer B: ");
-//Setting up pwn pin w/ servo
-sjsu::lpc40xx::Pwm p2_0(sjsu::lpc40xx::Pwm::Channel::kPwm0);
-ServoTask servo_one("Servo 1", p2_0);
+LEDTask led_one("Led 1");
+LEDTask led_two("Led 2");
 
 int main()
 {
@@ -108,19 +90,22 @@ int main()
 
   scheduler.AddTask(&printer_one);
   scheduler.AddTask(&printer_two);
-  scheduler.AddTask(&servo_one);
+  scheduler.AddTask(&led_one);
+  scheduler.AddTask(&led_two);
 
-  // setting Printer A to print 2 times faster than Printer B
   printer_one.SetDelayTime(500);
   printer_two.SetDelayTime(1000);
-  //Setting servo delay, hopefully its enough to move between rotations
-  servo_one.SetDelayTime(3000);
+  led_one.SetDelayTime(5000);
+  led_two.SetDelayTime(1000);
 
   sjsu::LogInfo("Attempting to search for Printer A in the scheduler...");
-  sjsu::rtos::TaskInterface * task = scheduler.GetTask("Printer A");
+  sjsu::rtos::TaskInterface *task = scheduler.GetTask("Printer A");
 
-  sjsu::LogInfo("Attempting to search for servo_one in the scheduler...");
-  sjsu::rtos::TaskInterface * task2 = scheduler.GetTask("Servo 1");
+  sjsu::LogInfo("Attempting to search for Led 1 in the scheduler...");
+  sjsu::rtos::TaskInterface *task2 = scheduler.GetTask("Led 1");
+
+  sjsu::LogInfo("Attempting to search for Led 1 in the scheduler...");
+  sjsu::rtos::TaskInterface *task3 = scheduler.GetTask("Led 2");
 
   if (task != nullptr)
   {
@@ -138,7 +123,17 @@ int main()
   }
   else
   {
-    sjsu::LogError("Could not find task Servo 1, Halt System.");
+    sjsu::LogError("Could not find task led 1, Halt System.");
+    sjsu::Halt();
+  }
+
+  if (task3 != nullptr)
+  {
+    sjsu::LogInfo("Found task: %s", task3->GetName());
+  }
+  else
+  {
+    sjsu::LogError("Could not find task led 2, Halt System.");
     sjsu::Halt();
   }
 
