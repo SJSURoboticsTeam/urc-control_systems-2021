acef14aee02838705579ec12bb1c0ef691c24079
naterpotatoers <nate.garza@hotmail.com>
2020-09-25 18:42:36 -0700

    adds initial walkthrough comments & new OLED Task object

--- Demos/template_application/source/main.cpp
+++ Demos/template_application/source/main.cpp
@@ -4,11 +4,16 @@
 #include "utility/log.hpp"
 #include "utility/rtos.hpp"
 
+// Super basic RTOS demo application.
+
+// 1. Create Task object that inherits from rtos Task.
 class LEDTask final : public sjsu::rtos::Task<1024>
 {
 public:
+  // Constructor for the rtos task. It needs a task_name argument and a scheduler priority level ranging from low to high.
   LEDTask(const char *task_name) : Task(task_name, sjsu::rtos::Priority::kMedium) {}
-
+  // Setup() and Run() are required functions within the rtos class.
+  // Setup() will be run initially during instanstiation.
   bool Setup() override
   {
     sjtwo::led0.SetAsOutput();
@@ -18,7 +23,7 @@ public:
     sjtwo::led1.SetLow();
     return true;
   }
-
+  // Run() will be the instrutctions to be ran over and over again.
   bool Run() override
   {
     sjtwo::led0.Toggle();
@@ -27,95 +32,46 @@ public:
   }
 };
 
-class PrinterTask final : public sjsu::rtos::Task<1024>
+// 1. Creating another Task object that inherits from rtos Task.
+class OLEDTask final : public sjsu::rtos::Task<1024>
 {
 public:
-  PrinterTask(const char *task_name, const char *message)
-      : Task(task_name, sjsu::rtos::Priority::kMedium),
-        message_(message),
-        run_count_(0)
-  {
-    print_mutex = NULL;
-  }
-
+  // Constructor for the rtos task. It needs a task_name argument and a scheduler priority level ranging from low to high.
+  OLEDTask(const char *task_name) : Task(task_name, sjsu::rtos::Priority::kMedium) {}
+  // Setup() and Run() are required functions within the rtos class. Setup() will be run initially during instanstiation.
   bool Setup() override
   {
-    if (print_mutex == NULL)
-    {
-      print_mutex = xSemaphoreCreateMutex();
-    }
-    sjsu::LogInfo("Completed Setup() for: %s", GetName());
     return true;
   }
-
-  bool PreRun() override
-  {
-    xSemaphoreTake(print_mutex, portMAX_DELAY);
-    sjsu::LogInfo("Completed PreRun() for: %s", GetName());
-    xSemaphoreGive(print_mutex);
-    return true;
-  }
-
+  // Run() will be the instrutctions to be ran over and over again.
   bool Run() override
   {
-    // The text displayed
-    run_count_ += 1;
-    xSemaphoreTake(print_mutex, portMAX_DELAY);
-    sjsu::LogInfo("%s: %lu", message_, run_count_);
-    xSemaphoreGive(print_mutex);
-    if (run_count_ == 10)
-    {
-      sjsu::LogInfo("Deleting: %s", GetName());
-      Delete();
-    }
     return true;
   }
-
-private:
-  inline static SemaphoreHandle_t print_mutex;
-
-  const char *message_;
-  uint32_t run_count_;
 };
 
+// 2. Declare a TaskScheduler object. It is responsible for keeping track of the tasks.
 sjsu::rtos::TaskScheduler scheduler;
-PrinterTask printer_one("Printer A", "Printer A: ");
-PrinterTask printer_two("Printer B", "Printer B: ");
+// 3. Declare the Task you wrote earlier. In this case I have two LED and 1 OLED objects for demonstration purposes.
 LEDTask led_one("Led 1");
 LEDTask led_two("Led 2");
+OLEDTask oled_one("Oled 1");
 
 int main()
 {
   sjsu::LogInfo("Starting TaskScheduler example...");
-
-  scheduler.AddTask(&printer_one);
-  scheduler.AddTask(&printer_two);
+  // 3. Add the Tasks to the scheduler. Think of it like loading in tasks to be managed by the scheduler.
   scheduler.AddTask(&led_one);
   scheduler.AddTask(&led_two);
-
-  printer_one.SetDelayTime(500);
-  printer_two.SetDelayTime(1000);
+  // 4. Can adjust the time alloted to each task between switching off from each other.
   led_one.SetDelayTime(5000);
   led_two.SetDelayTime(1000);
-
-  sjsu::LogInfo("Attempting to search for Printer A in the scheduler...");
-  sjsu::rtos::TaskInterface *task = scheduler.GetTask("Printer A");
-
+  // 5.
   sjsu::LogInfo("Attempting to search for Led 1 in the scheduler...");
   sjsu::rtos::TaskInterface *task2 = scheduler.GetTask("Led 1");
 
   sjsu::LogInfo("Attempting to search for Led 1 in the scheduler...");
   sjsu::rtos::TaskInterface *task3 = scheduler.GetTask("Led 2");
-
-  if (task != nullptr)
-  {
-    sjsu::LogInfo("Found task: %s", task->GetName());
-  }
-  else
-  {
-    sjsu::LogError("Could not find task \"Printer A\", Halt System.");
-    sjsu::Halt();
-  }
 
   if (task2 != nullptr)
   {
