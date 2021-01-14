9470f4f1f9a47104b4bcfccdc4954376f62aea46
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2021-01-14 10:12:49 -0800

    Arm control prototype (#24)
    * Add initial Arm Class.

---
+++ Arm/hand.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "L1_Peripheral/uart.hpp"
+namespace sjsu::arm
+{
+class Hand
+{
+  /// The hand has its own MCU that communicates with the arm via UART.
+  
+ public:
+  Hand(Uart & uart) : uart_(uart) {}
+  void Initialize() {}
+  void Enable(bool enable = true) {}
+ private:
+  Uart & uart_;
+};
+}  // namespace sjsu::arm
