51e52124bbfa6c959b2d4def190b90368a933755
Joshua Ramayrat <joshua@ramayrat.com>
2021-02-25 13:12:02 -0800

    added some content to drive main

--- Drive/source/main.cpp
+++ Drive/source/main.cpp
@@ -9,12 +9,38 @@
 int main(void)
 {
   sjsu::LogInfo("Starting the rover drive system...");
-  // Not sure how to implement
-  // sjsu::lpc40xx::Can can_network(sjsu::lpc40xx::Can::Channel::kCan2);
-  // sjsu::RmdX left_hub_motor(can_network, 0x140);
-  // address 0x140 - 0x148
-  // sjsu::RmdX left_steer_motor(can_network, 0x148);
-  // sjsu::drive::Wheel left_wheel(left_hub_motor, left_steer_motor);
+
+  sjsu::lpc40xx::Can can(sjsu::lpc40xx::Can::Channel::kCan2);
+  sjsu::StaticAllocator<1024> memory_resource;
+  sjsu::CanNetwork can_network(can, &memory_resource);
+
+  sjsu::lpc40xx::Can can_network(sjsu::lpc40xx::Can::Channel::kCan2);
+  sjsu::RmdX left_hub_motor(can_network, 0x140);
+  sjsu::RmdX left_steer_motor(can_network, 0x148);
+
+  sjsu::RmdX right_hub_motor(can_network, 0x149);
+  sjsu::RmDX right_steer_motor(can_network, 0x14A);
+
+  sjsu::RmDX back_hub_motor(can_network, 0x14B);
+  sjsu::RmDX back_steer_motor(can_network, 0x14C);
+
+
+  sjsu::drive::Wheel left_wheel(left_hub_motor, left_steer_motor);
+  sjsu::drive::Wheel back_wheel(left_hub_motor, left_steer_motor);
+  sjsu::drive::Wheel right_wheel(left_hub_motor, left_steer_motor);
+
+  sjsu::arm::RoverDriveSystem driveControl(left_wheel, right_wheel, back_wheel);
+
+  // still need to include accelerometers, encoders, etc
+
+  driveControl.Initialize();
+  driveControl.Enable();
+  driveControl.Home();
+
+  while(true)
+  {
+
+  }
 
   return 0;
 }
