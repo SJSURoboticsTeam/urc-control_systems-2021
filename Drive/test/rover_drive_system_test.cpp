1d4fbd1c6ced93a78d0f55c6689657fe4cb03082
naterpotatoers <nate.garza@hotmail.com>
2021-03-02 18:25:52 -0800

    adds rmd mock

--- Drive/test/rover_drive_system_test.cpp
+++ Drive/test/rover_drive_system_test.cpp
@@ -4,5 +4,21 @@
 
 namespace sjsu
 {
-TEST_CASE("Drive System") {}
+TEST_CASE("Drive System")
+{
+  Mock<Can> mock_can;
+  Fake(Method(mock_can, Can::ModuleInitialize));
+  Fake(OverloadedMethod(mock_can, Can::Send, void(const Can::Message_t &)));
+  Fake(Method(mock_can, Can::Receive));
+  Fake(Method(mock_can, Can::HasData));
+  StaticMemoryResource<1024> memory_resource;
+  CanNetwork network(mock_can.get(), &memory_resource);
+
+  constexpr auto kId = 0x140;
+  RmdX rmd_left_1(network, kId);
+  SECTION("setup")
+  {
+    rmd_left_1.Initialize();
+  }
+}
 }  // namespace sjsu
