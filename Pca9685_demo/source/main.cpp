765b0dba958e3d443db5aa2398880b5212e26850
Andrew E <32277829+AndrewDEmerson@users.noreply.github.com>
2021-04-18 21:38:41 -0700

    Add pca9685 driver.

---
+++ Pca9685_demo/source/main.cpp
@@ -0,0 +1,21 @@
+#include "../Arm/pca9685.hpp"
+#include "peripherals/lpc40xx/i2c.hpp"
+
+int main(void)
+{
+  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<2>();
+  sjsu::Pca9685 pca(i2c);
+  pca.ModuleInitialize();
+  sjsu::LogInfo("Starting program");
+
+
+  while (true)
+  {
+    pca.setPulseWidth(0,500_us);
+    sjsu::Delay(1.5_s);
+    pca.setPulseWidth(0,2500_us);
+
+    sjsu::Delay(1.5_s);
+    sjsu::LogInfo("ran program");
+  }
+}
