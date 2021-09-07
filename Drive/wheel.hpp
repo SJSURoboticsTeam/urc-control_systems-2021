c578eb9c28666f8b99bbc27b2e180375413e1ba9
Maaz0070 <67044481+Maaz0070@users.noreply.github.com>
2021-09-06 18:21:59 -0700

    Update wheel.hpp

--- Drive/wheel.hpp
+++ Drive/wheel.hpp
@@ -46,56 +46,8 @@ class Wheel
     {
        sjsu::LogInfo("made it to sethubspeed()");
        
-       auto clampedHubSpeed = std::clamp(hub_speed, kMaxNegSpeed, kMaxPosSpeed);
-      //static cast to use clampedHubSpeed in lerp function
-      long double new_speed = static_cast<long double>(clampedHubSpeed);
-      //intialize the variable we will use to store the revolutions_per_minute_t that is returned on each call to std::lerp
-      long double lerp_speed = 0;
-      //intialize the variable we will use to store the current speed of the motor after each call to lerp
-      long double current_speed = 0;
-
-      //want to make sure we are supposed to increase or decrease the speed when setting the speed
-      bool increaseSpeed = true;
-      if(hub_motor_.RequestFeedbackFromMotor().GetFeedback().speed > hub_speed)
-      {
-        increaseSpeed = false;
-      }
-
-      //lerp must continue as long as input speed(hub_speed) does not equal the output of the lerp function(lerp_speed) 
-       while(hub_speed != std::clamp(units::angular_velocity::revolutions_per_minute_t{lerp_speed}, kMaxNegSpeed, hub_speed))
-       {
-        auto next_same_time = Uptime() + 100ms;
-
-        //current_speed is pulled from the motor feedback and casted to long double 
-        current_speed = static_cast<long double>(hub_motor_.RequestFeedbackFromMotor().GetFeedback().speed);
-
-        //lerp returns a midpoint between current_speed and the new_speed
-        lerp_speed = std::lerp(current_speed, new_speed, static_cast<long double>(0.5));
-
-        
-        //Set the speed of the motor and convert long double lerp_speed to revolutions_per_minute_t object
-        //use std::clamp to make sure we don't set the speed higher/lower than the input speed.
-        //When using std::clamp check if the fucntion is increasing or decreasing the speed to limit the max/min speeds
-        if(increaseSpeed)
-        {
-            hub_motor_.SetSpeed(std::clamp(units::angular_velocity::revolutions_per_minute_t{lerp_speed}, kMaxNegSpeed, hub_speed));
-        }
-        else
-        {
-            hub_motor_.SetSpeed(std::clamp(units::angular_velocity::revolutions_per_minute_t{lerp_speed}, hub_speed, kMaxPosSpeed));
-        }
-        
-
-
-        sjsu::LogInfo("Set hub speed to %f", lerp_speed);
-
-        //make sure 100ms have passed before setting the next speed
-        while(Uptime() < next_same_time)
-        {
-          continue;
-        }
-
-       }
+       hub_motor_.SetSpeed(hub_speed);
+       hub_speed_ = hub_speed;
        
     }
     catch (const std::exception & e)
