b294b7595365428035a9933b817b75a231500dd3
naterpotatoers <nate.garza@hotmail.com>
2021-09-15 16:17:26 -0700

    Adds new arm control loop & updates drives control loop slightly for neatness

--- Arm/source/main.cpp
+++ Arm/source/main.cpp
@@ -1,53 +1,75 @@
 #include "utility/log.hpp"
-#include "RoverArmSystem.hpp"
 #include "peripherals/lpc40xx/i2c.hpp"
 #include "peripherals/lpc40xx/can.hpp"
+#include "utility/time/timeout_timer.hpp"
 #include "devices/actuators/servo/rmd_x.hpp"
 #include "devices/sensors/movement/accelerometer/mpu6050.hpp"
 
+#include "RoverArmSystem.hpp"
+#include "../../Common/esp.hpp"
+
 int main()
 {
-  // // Create a can bus for the RMD_x7 motors to use and all of the RMD_x7
-  // motor objects for the arm.
-  // sjsu::lpc40xx::Can can(sjsu::lpc40xx::Can::Channel::kCan2);
-  // sjsu::StaticAllocator<1024> memory_resource;
-  // sjsu::CanNetwork can_network(can, &memory_resource);
-  // sjsu::RmdX rmd_rotunda(can_network, 0x148);
-  // sjsu::RmdX rmd_shoulder(can_network, 0x149);
-  // sjsu::RmdX rmd_elbow(can_network, 0x14A);
-  // sjsu::RmdX rmd_left_wrist(can_network, 0x14B);
-  // sjsu::RmdX rmd_right_wrist(can_network, 0x14C);
+  sjsu::LogInfo("Starting the rover arm system...");
+  sjsu::common::Esp esp;
+  sjsu::lpc40xx::Can & can = sjsu::lpc40xx::GetCan<2>();
+  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<2>();
+  sjsu::StaticMemoryResource<1024> memory_resource;
+  sjsu::CanNetwork can_network(can, &memory_resource);
 
-  // // Create an I2C object for the IMU's as well as all of the MPU objects for
-  // // the arm.
-  // //
-  // // Note that this only works if an address translator is used for the
-  // // MPU6050s; as they can only be set to use two addresses. If the chip
-  // select
-  // // method is used then they would all be set to the same address and
-  // pulling
-  // // the address pin HIGH/LOW would select the correct MPU to access.
-  // sjsu::lpc40xx::I2c i2c(sjsu::lpc40xx::I2c::Bus::kI2c2);
-  // sjsu::Mpu6050 mpu_rotunda(i2c, 0x68);
-  // sjsu::Mpu6050 mpu_shoulder(i2c, 0x69);
-  // sjsu::Mpu6050 mpu_elbow(i2c, 0x6A);
-  // sjsu::Mpu6050 mpu_wrist(i2c, 0x6B);
+  // Need to use an address translator
+  sjsu::Mpu6050 rotunda_mpu(i2c, 0x68);
+  sjsu::Mpu6050 shoulder_mpu(i2c, 0x69);
+  sjsu::Mpu6050 elbow_mpu(i2c, 0x6A);
+  sjsu::Mpu6050 wrist_mpu(i2c, 0x6B);
 
-  // // Attach the RMD_x7 motor objects and the MPUs to the appropriate arm
-  // joint. sjsu::arm::Joint rotunda(rmd_rotunda, mpu_rotunda, 0_deg, 3600_deg,
-  // 1800_deg); sjsu::arm::Joint shoulder(rmd_shoulder, mpu_shoulder);
-  // sjsu::arm::Joint elbow(rmd_elbow, mpu_elbow);
-  // sjsu::arm::WristJoint wrist(rmd_left_wrist, rmd_right_wrist, mpu_wrist);
+  // RMD addresses 0x141 - 0x148 are available
+  sjsu::RmdX rotunda_motor(can_network, 0x141);
+  sjsu::RmdX shoulder_motor(can_network, 0x142);
+  sjsu::RmdX elbow_motor(can_network, 0x143);
+  sjsu::RmdX left_wrist_motor(can_network, 0x144);
+  sjsu::RmdX right_wrist_motor(can_network, 0x145);
 
-  // // Attach the Joins to the arm controller object.
-  // sjsu::arm::RoverArmSystem armControl(rotunda, shoulder, elbow, wrist);
-  // armControl.Initialize();
-  // armControl.Home();
+  rotunda_motor.settings.gear_ratio     = 8;
+  shoulder_motor.settings.gear_ratio    = 8;
+  elbow_motor.settings.gear_ratio       = 8;
+  left_wrist_motor.settings.gear_ratio  = 8;
+  right_wrist_motor.settings.gear_ratio = 8;
 
-  // while (true)
-  // {
-  //   armControl.GetData();
-  //   armControl.MoveArm();
-  //   sjsu::Delay(100ms);
-  // }
+  sjsu::arm::Joint rotunda(rotunda_motor, rotunda_mpu, 0_deg, 3600_deg,
+                           1800_deg);
+  sjsu::arm::Joint shoulder(shoulder_motor, shoulder_mpu);
+  sjsu::arm::Joint elbow(elbow_motor, elbow_mpu);
+  sjsu::arm::WristJoint wrist(left_wrist_motor, right_wrist_motor, wrist_mpu);
+  sjsu::arm::RoverArmSystem arm(rotunda, shoulder, elbow, wrist);
+
+  esp.Initialize();
+  arm.Initialize();
+
+  while (1)
+  {
+    try
+    {
+      sjsu::LogInfo("Making new request...");
+      std::string endpoint = "arm?example=1&param=2";  // include status updates
+      std::string response = esp.GETRequest(endpoint);
+      sjsu::TimeoutTimer serverTimeout(5s);  // server has 5s timeout
+      // Do stuff with arm here...
+      sjsu::Delay(3s);
+      if (serverTimeout.HasExpired())
+      {
+        sjsu::LogWarning("Server timed out! Reconnecting...");
+        esp.ConnectToServer();
+      }
+    }
+    catch (const std::exception & e)
+    {
+      sjsu::LogError("Uncaught error in main()!");
+      if (!esp.IsConnected())
+      {
+        esp.ConnectToWifi();
+        esp.ConnectToServer();
+      }
+    }
+  }
 }
