0ae81372402e684e4036edc775defa3cd0154308
Gene Luong <luonggene@gmail.com>
2021-12-21 20:22:47 -0700

    made state of charge it's own file

--- Drive/rover_drive_system.hpp
+++ Drive/rover_drive_system.hpp
@@ -7,17 +7,9 @@
 #include "utility/math/units.hpp"
 #include "utility/math/map.hpp"
 
+#include "../Common/state_of_charge.hpp"
 #include "../Common/esp.hpp"
 #include "wheel.hpp"
-
-#include "../../../library/devices/sensors/battery/max17043.hpp"
-// #include "../../../libray  "
-#include "../../../library/peripherals/lpc17xx/i2c.hpp"
-//#include "../../../library/lpc40xx/peripherals/gpio.hpp"
-#include "peripherals/lpc40xx/gpio.hpp"
-#include "../../../library/peripherals/hardware_counter.hpp"
-#include "../../../library/devices/sensors/battery/ltc4150.hpp" 
-
 
 namespace sjsu::drive
 {
@@ -178,41 +170,6 @@ class RoverDriveSystem
       throw e;
     }
   };
-
-double StateOfCharge_MAX(){
-  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<0>();
-  i2c.Initialize();
-  sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 1>();
-  alert_pin.Initialize();
-  bool callback_was_called   = false;
-        InterruptCallback callback = [&callback_was_called]() {
-          callback_was_called = true;
-        };
-  uint8_t address = 0b0110110;
-
-  Max170343 *batterySensor = new Max170343(i2c, alert_pin, callback, address);
-
-  units::voltage::volt_t batteryVoltage = batterySensor -> GetVoltage();
-  double Voltage = batteryVoltage.to<double>();
-  return Voltage/3.3;
-
-}
-
-double StateOfCharge_LTC(){
-  
-  sjsu::lpc40xx::Gpio &gpio = sjsu::lpc40xx::GetGpio<0, 1>();
-  sjsu::GpioCounter counter(gpio, sjsu::Gpio::Edge::kRising);
-
-  sjsu::lpc40xx::Gpio &pol = sjsu::lpc40xx::GetGpio<2, 3>();
-  units::impedance::ohm_t resistance = 10'000_Ohm;
-
-  Ltc4150 *batterySensor = new Ltc4150(counter,pol,resistance);
-
-  units::charge::microampere_hour_t batteryCharge = batterySensor->GetCharge();
-  double Charge = batteryCharge.to<double>();
-  return Charge/100.0;
-
-}
 
   /// Prints the mission control data & prints the current speed and steer angle
   /// of each wheel on the rover
@@ -424,8 +381,6 @@ double StateOfCharge_LTC(){
   };
 
   char current_mode_   = 'S';
-  int state_of_charge_ = static_cast<int>(StateOfCharge_MAX());  // TODO - hardcoded for now
-  int state_of_charge_LTC = static_cast<int>(StateOfCharge_LTC());
   const units::angular_velocity::revolutions_per_minute_t kZeroSpeed = 0_rpm;
 
  public:
@@ -433,5 +388,8 @@ double StateOfCharge_LTC(){
   Wheel & left_wheel_;
   Wheel & right_wheel_;
   Wheel & back_wheel_;
+
+  int state_of_charge_MAX = static_cast<int>(StateOfCharge_MAX());
+  int state_of_charge_LTC = static_cast<int>(StateOfCharge_LTC());
 };
 }  // namespace sjsu::drive
