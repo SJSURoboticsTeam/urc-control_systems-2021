4a62bea999b102f0a100ba22f5c3ad4502de40ba
Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
2022-01-10 16:46:12 -0800

    Fixes improper merge (#87)
    * Unit Change To Floats, Cleaned up Units in Wrist_Joint
    
    * fixes improper merge

--- Arm/joint.hpp
+++ Arm/joint.hpp
@@ -99,6 +99,7 @@ class Joint
   units::angle::degree_t maximum_angle     = 180_deg;
   units::angle::degree_t rest_angle        = 0_deg;
   units::angle::degree_t zero_offset_angle = 0_deg;
+  const float kLerpStep = .5;
   sjsu::RmdX & motor;
   sjsu::Mpu6050 & mpu;
   units::angular_velocity::revolutions_per_minute_t speed_     = 0_rpm;
