38c25d3df2b7a30f815bab09f6de235011d3129d
mchan2003 <mchan100work@gmail.com>
2022-01-22 13:29:06 -0800

    set max_angle to 180 and formated the files

--- Arm/Hand/finger.hpp
+++ Arm/Hand/finger.hpp
@@ -6,56 +6,56 @@ namespace sjsu::arm
 {
 class Finger
 {
-public:
+ public:
+  Finger(sjsu::Servo & servo) : servo_(servo) {}
 
-Finger(sjsu::Servo & servo) : servo_(servo)
-{}
+  Finger(sjsu::Servo & servo,
+         float position,
+         float speed,
+         float max_angle,
+         float min_angle)
+      : servo_(servo),
+        position_(position),
+        speed_(speed),
+        max_angle_(max_angle),
+        min_angle_(min_angle)
+  {
+  }
 
-Finger(sjsu::Servo & servo,
-        float position,
-        float speed,
-        float max_angle,
-        float min_angle
-       ) : servo_(servo), position_(position), speed_(speed), max_angle_(max_angle), min_angle_(min_angle)
-       {}
-
-void Initialize()
-{
+  void Initialize()
+  {
     servo_.ModuleInitialize();
-}
+  }
 
-void SetPosition(float angle)
-{
+  void SetPosition(float angle)
+  {
     position_ = std::clamp(angle, min_angle_, max_angle_);
     units::angle::degree_t angle_to_degrees(position_);
     servo_.SetAngle(angle_to_degrees);
-};
+  };
 
-void SetSpeed(float target_speed)
-{
+  void SetSpeed(float target_speed)
+  {
     speed_ = target_speed;
-};
+  };
 
-int GetPosition()
-{
+  int GetPosition()
+  {
     return int(position_);
-};
+  };
 
-int GetSpeed()
-{
+  int GetSpeed()
+  {
     return int(speed_);
+  };
+
+ private:
+  sjsu::Servo & servo_;
+
+  float position_  = 0;
+  float speed_     = 0;
+  float max_angle_ = 180;
+  float min_angle_ = 0;
 };
 
-
-private:
-
- sjsu::Servo & servo_;
-
- float position_  = 0;
- float speed_     = 0;
- float max_angle_ = 0;
- float min_angle_ = 0;
-
-};
-
-}//sjsu::arm
+}  // namespace sjsu::arm
