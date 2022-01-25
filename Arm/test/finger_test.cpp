86bdd9c9e8b550c1c6a7bd0f0895fa3b882b7ae3
AndrewI <andrewing777@gmail.com>
2022-01-24 18:09:00 -0800

    created unit tests for finger

--- Arm/test/finger_test.cpp
+++ Arm/test/finger_test.cpp
@@ -6,13 +6,39 @@ namespace sjsu::arm
 {
 TEST_CASE("Finger Testing Case...")
 {
-    Mock<Servo> mock_servo;
+  Mock<Servo> mock_servo;
 
-    Finger finger(mock_servo.get());
+  Finger finger(mock_servo.get());
 
-    SECTION("Should return 0 when called...")
-    {
-        CHECK_EQ(finger.GetPosition(), 0);
-    }
+  SECTION("Should return 0 when finger is first initialized")
+  {
+    CHECK_EQ(finger.GetPosition(), 0);
+  }
+
+  SECTION("Should return 0, 10, 45, 90, 100 when SetPosition() is called")
+  {
+    CHECK_EQ(finger.GetPosition(), 0);
+    finger.SetPosition(10);
+    CHECK_EQ(finger.GetPosition(), 10);
+    finger.SetPosition(45);
+    CHECK_EQ(finger.GetPosition(), 45);
+    finger.SetPosition(90);
+    CHECK_EQ(finger.GetPosition(), 90);
+    finger.SetPosition(100);
+    CHECK_EQ(finger.GetPosition(), 100);
+    finger.SetPosition(0);
+    CHECK_EQ(finger.GetPosition(), 0);
+  }
+
+  SECTION("Boundry testing the angles")
+  {
+    max_angle_ = 180;
+    min_angle_ = 0;
+    CHECK_EQ(finger.GetPosition(), 0);
+    finger.SetPosition(-1);
+    CHECK_EQ(finger.GetPosition(), 0);
+    finger.SetPosition(181);
+    CHECK_EQ(finger.GetPosition(), 180);
+  }
 }
-}//sjsu
+}  // namespace sjsu::arm
