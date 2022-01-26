9efa598bcf84f29a67ddc585da8b85cd3fdd4e26
AndrewI <andrewing777@gmail.com>
2022-01-25 21:21:24 -0800

    re did unit tests

--- Arm/test/finger_test.cpp
+++ Arm/test/finger_test.cpp
@@ -10,27 +10,12 @@ TEST_CASE("Finger Testing Case...")
 
   Finger finger(mock_servo.get());
 
-  SECTION("Should return 0 when finger is first initialized")
+  SECTION("1.1 Should return 0 when finger is first initialized")
   {
     CHECK_EQ(finger.GetPosition(), 0);
   }
 
-  SECTION("Should return 0, 10, 45, 90, 100 when SetPosition() is called")
-  {
-    CHECK_EQ(finger.GetPosition(), 0);
-    finger.SetPosition(10);
-    CHECK_EQ(finger.GetPosition(), 10);
-    finger.SetPosition(45);
-    CHECK_EQ(finger.GetPosition(), 45);
-    finger.SetPosition(90);
-    CHECK_EQ(finger.GetPosition(), 90);
-    finger.SetPosition(100);
-    CHECK_EQ(finger.GetPosition(), 100);
-    finger.SetPosition(0);
-    CHECK_EQ(finger.GetPosition(), 0);
-  }
-
-  SECTION("Boundry testing the angles")
+  SECTION("2.1 - 2.4 Boundry testing the angles")
   {
     int max_angle_ = 180;
     int min_angle_ = 0;
