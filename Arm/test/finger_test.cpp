1cdc760ba5ead5ce8d9beb140efa2a63ad3149a4
AndrewI <andrewing777@gmail.com>
2022-02-01 20:20:34 -0800

    seperated unit tests

--- Arm/test/finger_test.cpp
+++ Arm/test/finger_test.cpp
@@ -15,15 +15,20 @@ TEST_CASE("Finger Testing Case...")
     CHECK_EQ(finger.GetPosition(), 0);
   }
 
-  SECTION("2.1 - 2.4 Boundry testing the angles")
+  SECTION("2.1 Boundry testing the upper bounds")
   {
     int max_angle_ = 180;
+    
+    CHECK_EQ(finger.GetPosition(), 0);
+    finger.SetPosition(181);
+    CHECK_EQ(finger.GetPosition(), 180);
+  }
+  SECTION("2.2 Boundry testing the upper bounds")
+  {
     int min_angle_ = 0;
     CHECK_EQ(finger.GetPosition(), 0);
     finger.SetPosition(-1);
     CHECK_EQ(finger.GetPosition(), 0);
-    finger.SetPosition(181);
-    CHECK_EQ(finger.GetPosition(), 180);
   }
 }
 }  // namespace sjsu::arm
