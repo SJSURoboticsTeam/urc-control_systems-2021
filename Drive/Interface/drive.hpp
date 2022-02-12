c2b21170045725d26ef6bc2ed4cefae75ed48273
Gene Luong <luonggene@gmail.com>
2022-02-12 13:50:36 -0700

    updated drive functions

--- Drive/Interface/drive.hpp
+++ Drive/Interface/drive.hpp
@@ -4,16 +4,24 @@ namespace sjsu::drive
 
 class drive{
     virtual void SwitchLegOrientation() = 0;
-    virtual bool IsNewMode()
-    virtual Modes GetCurrentMode()
-    virtual void StopWheels()
-    virtual bool IsStopped()
-    virtual void SetWheelSpeed()
-    virtual void HomeWheels()
-    virtual bool AllWheelsAreHomed()
-    virtual void SetMode()
-    virtual void SetDriveMode()
-    
+    virtual bool IsNewMode() = 0;
+    virtual Modes GetCurrentMode() = 0;
+    virtual void StopWheels() = 0;
+    virtual bool IsStopped() = 0;
+    virtual void SetWheelSpeed() = 0;
+    virtual void HomeWheels() = 0;
+    virtual bool AllWheelsAreHomed() = 0;
+    virtual void SetMode() = 0;
+    virtual void SetDriveMode() = 0;
+    virtual void SetSpinMode() = 0;
+    virtual void SetTranslationMode() = 0;
+    virtual void SetSingleWheelMode()
+    virtual void HandleDriveMode() = 0;
+    virtual float GetOutterWheelDriveAngle() = 0;
+    virtual float GetBackWheelDriveAngle() = 0;
+    virtual void HandleSpinMode() = 0;
+    virtual void HandleTranslationMode() = 0;
+    virtual void HandleSingularWheelMode() = 0;
 }
 
 }  // namespace sjsu::drive
