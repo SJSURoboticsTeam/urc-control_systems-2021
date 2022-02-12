5efa91788068c696470a56f2617ded8a07ec3480
Gene Luong <luonggene@gmail.com>
2022-02-12 14:05:20 -0700

    removed the virutal

--- Drive/Interface/drive.hpp
+++ Drive/Interface/drive.hpp
@@ -1,7 +1,7 @@
 #pragma once
 namespace sjsu::drive
 {
-  
+
 enum class Modes : char
   {
     DriveMode      = 'D',
@@ -13,25 +13,25 @@ enum class Modes : char
   };
 
 class drive{
-    virtual void SwitchLegOrientation() = 0;
-    virtual bool IsNewMode() = 0;
-    virtual Modes GetCurrentMode() = 0;
-    virtual void StopWheels() = 0;
-    virtual bool IsStopped() = 0;
-    virtual void SetWheelSpeed() = 0;
-    virtual void HomeWheels() = 0;
-    virtual bool AllWheelsAreHomed() = 0;
-    virtual void SetMode() = 0;
-    virtual void SetDriveMode() = 0;
-    virtual void SetSpinMode() = 0;
-    virtual void SetTranslationMode() = 0;
-    virtual void SetSingleWheelMode() = 0;
-    virtual void HandleDriveMode() = 0;
-    virtual float GetOutterWheelDriveAngle() = 0;
-    virtual float GetBackWheelDriveAngle() = 0;
-    virtual void HandleSpinMode() = 0;
-    virtual void HandleTranslationMode() = 0;
-    virtual void HandleSingularWheelMode() = 0;
-}
+    void SwitchLegOrientation();
+    bool IsNewMode();
+    Modes GetCurrentMode();
+    void StopWheels();
+    bool IsStopped();
+    void SetWheelSpeed();
+    void HomeWheels();
+    bool AllWheelsAreHomed();
+    void SetMode();
+    void SetDriveMode();
+    void SetSpinMode();
+    void SetTranslationMode();
+    void SetSingleWheelMode();
+    void HandleDriveMode();
+    float GetOutterWheelDriveAngle();
+    float GetBackWheelDriveAngle();
+    void HandleSpinMode();
+    void HandleTranslationMode();
+    void HandleSingularWheelMode();
+};
 
 }  // namespace sjsu::drive
