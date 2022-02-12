6da474b283e9625309cb1dcee90a30b81b432a82
Gene Luong <luonggene@gmail.com>
2022-02-12 13:41:04 -0700

    updated drive functions

--- Drive/Interface/drive.hpp
+++ Drive/Interface/drive.hpp
@@ -2,9 +2,18 @@
 namespace sjsu::drive
 {
 
-class drive
-{
-  virtual void test() = 0;
-};
+class drive{
+    virtual void SwitchLegOrientation() = 0;
+    virtual bool IsNewMode()
+    virtual Modes GetCurrentMode()
+    virtual void StopWheels()
+    virtual bool IsStopped()
+    virtual void SetWheelSpeed()
+    virtual void HomeWheels()
+    virtual bool AllWheelsAreHomed()
+    virtual void SetMode()
+    virtual void SetDriveMode()
+    
+}
 
 }  // namespace sjsu::drive
