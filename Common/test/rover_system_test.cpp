47df55660ec5fdec04b02db2a5dc491c7e4b52dd
naterpotatoers <nate.garze@hotmail.com>
2022-03-09 22:14:01 -0800

    adds unit tests for rover_system_interface

--- Common/test/rover_system_test.cpp
+++ Common/test/rover_system_test.cpp
@@ -9,6 +9,7 @@ TEST_CASE("Rover system testing")
 {
   class TestSystem : public RoverSystemInterface
   {
+   public:
     void Initialize(){};
     void PrintRoverData(){};
     std::string CreateGETRequestParameterWithRoverStatus()
@@ -20,13 +21,36 @@ TEST_CASE("Rover system testing")
       response = "";
     };
     void HandleRoverCommands(){};
+
+    RoverMissionControlData mc_data_;
   };
 
   TestSystem rover;
 
-  SECTION("1.1 should return zero heartbeat count")
+  SECTION("1.1 should verify default values")
   {
-    CHECK_EQ(rover.GetHeartbeatCount(), 0);
+    CHECK_EQ(rover.mc_data_.is_operational, 0);
+    CHECK_EQ(rover.mc_data_.heartbeat_count, 0);
+  }
+
+  SECTION("2.1 should verify rover is operational when passed 1")
+  {
+    CHECK(rover.IsOperational(1));
+  }
+
+  SECTION("2.2 should verify rover is not operational when passed 0")
+  {
+    CHECK_FALSE(rover.IsOperational(0));
+  }
+
+  SECTION("2.3 should verify rover is not operational when passed -1")
+  {
+    CHECK_FALSE(rover.IsOperational(-1));
+  }
+
+  SECTION("2.4 should verify rover is not operational when passed 2")
+  {
+    CHECK_FALSE(rover.IsOperational(2));
   }
 }
 }  // namespace sjsu::common
