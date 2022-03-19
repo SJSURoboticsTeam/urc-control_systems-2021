1e185558a55808b9e44b92a751e52f7d93a7f2e1
Andrew-Ingerman <andrewing777@gmail.com>
2022-03-19 13:21:18 -0700

    added initial methods

--- Arm-Subsystem/joint_interface.hpp
+++ Arm-Subsystem/joint_interface.hpp
@@ -2,8 +2,14 @@
 
 namespace sjsu::arm
 {
-    class joint_interface
-    {
-        
-    }
+class joint_interface
+{
+  struct accelerometer
+  {
+  } 
+  virtual void Initialize()             = 0;
+  virtual void SetPosition(float angle) = 0;
+  virtual int GetSpeed()                = 0;
+  virtual int GetPosition()             = 0;
 }
+}  // namespace sjsu::arm
