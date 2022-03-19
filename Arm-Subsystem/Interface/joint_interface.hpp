b06b3af4cd6cc61f9ac8ab646c1b7d05e6094ea5
mchan2003 <mchan100work@gmail.com>
2022-03-19 13:45:54 -0700

    renamed and move files

--- Arm-Subsystem/Interface/joint_interface.hpp
+++ Arm-Subsystem/Interface/joint_interface.hpp
@@ -1,15 +1,19 @@
-
+#pragma once
+#include "devices/sensors/movement/accelerometer/mpu6050.hpp"
+#include "devices/actuators/servo/rmd_x.hpp"
+#include "Common/accelerometer.hpp"
 
 namespace sjsu::arm
 {
-class joint_interface
+class Joint // interface
 {
-  struct accelerometer
-  {
-  } 
+
   virtual void Initialize()             = 0;
   virtual void SetPosition(float angle) = 0;
   virtual int GetSpeed()                = 0;
   virtual int GetPosition()             = 0;
-}
+  
+  protected:
+  sjsu::Common::Accelerometer test;
+};
 }  // namespace sjsu::arm
