c48b52745b7bb2222fa55ac2b4903d7cfc5d0499
Andrew-Ingerman <andrewing777@gmail.com>
2022-03-26 13:21:33 -0700

    reversed a couple of Gear Ratios

--- Arm-Subsystem/source/main.cpp
+++ Arm-Subsystem/source/main.cpp
@@ -39,8 +39,8 @@ int main()
   sjsu::arm::Finger thumb(0);
 
   rotunda_motor.settings.gear_ratio     = 8;
-  shoulder_motor.settings.gear_ratio    = 8*65/16;  //gear ratio of motor times gear ratio of shoulder
-  elbow_motor.settings.gear_ratio       = 8*5/2;        //gear ratio of motor times gear ratio of elbow
+  shoulder_motor.settings.gear_ratio    = 8*16/65;  //gear ratio of motor times gear ratio of shoulder
+  elbow_motor.settings.gear_ratio       = 8*2/5;        //gear ratio of motor times gear ratio of elbow
   left_wrist_motor.settings.gear_ratio  = 8;
   right_wrist_motor.settings.gear_ratio = 8;
 
