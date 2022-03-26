1300dca18e7a4d548f7b1d50d21c441ee768e204
Gene Luong <luonggene@gmail.com>
2022-03-26 13:22:30 -0700

    tested code seems to work, though it reads voltage from computer's usb-c port

--- Common/state_of_charge.hpp
+++ Common/state_of_charge.hpp
@@ -13,10 +13,11 @@ public:
 void Initialize(){
   //TODO - impliment initialize for state of charge
 }
+
 double StateOfCharge_MAX(){
-  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<0>();
+  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<2>();
   i2c.Initialize();
-  sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 1>();
+  sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 6>();
   alert_pin.Initialize();
   bool callback_was_called   = false;
         InterruptCallback callback = [&callback_was_called]() {
@@ -28,28 +29,21 @@ double StateOfCharge_MAX(){
 
   units::voltage::volt_t battery_voltage = battery_sensor -> GetVoltage();
   double voltage = battery_voltage.to<double>();
-  return voltage/3.3;
+
+  if(voltage < 3.05)
+  {
+    sjsu::LogWarning("Cell Voltage below 3. Battery dangerously low!");
+  }
+  else if(voltage < 3.25 )
+  {
+    sjsu::LogWarning("Cell Voltage below 3.25. Battery is low");
+  }
+
+    voltage = (voltage-3)/(4.2 - 3);
+
+    voltage = voltage*100;
+  return voltage;
 
 }
-
-double StateOfCharge_LTC(){
-  
-  sjsu::lpc40xx::Gpio &gpio = sjsu::lpc40xx::GetGpio<0, 1>();
-  sjsu::GpioCounter counter(gpio, sjsu::Gpio::Edge::kRising);
-
-  sjsu::lpc40xx::Gpio &pol = sjsu::lpc40xx::GetGpio<2, 3>();
-  units::impedance::ohm_t resistance = 10'000_Ohm;
-
-  Ltc4150 *battery_sensor = new Ltc4150(
-                             counter,
-                             pol,
-                             resistance);
-
-  units::charge::microampere_hour_t battery_charge = battery_sensor->GetCharge();
-  double charge = battery_charge.to<double>();
-  return charge/100.0; // 100 is a temporary and will need to be updated when we know the battery mA/h
-
-}
-
-  };
+};
 }  // namespace sjsu::common
---
+++ Demos/state_of_charge/source/main.cpp
@@ -0,0 +1,22 @@
+#include "utility/log.hpp"
+#include "../../Common/state_of_charge.hpp"
+
+int main(void)
+{
+  while (1)
+  {
+    try
+    {
+      sjsu::common::StateOfCharge st;
+      sjsu::Delay(1000ms);
+      sjsu::LogInfo("Max: %f%% remaining", st.StateOfCharge_MAX());
+     // esp.IsServerExpired(serverTimeout);
+    }
+    catch (const std::exception & e)
+    {
+      sjsu::LogError("Uncaught error in main() - Stopping Rover!");
+    }
+  }
+
+  return 0;
+}
