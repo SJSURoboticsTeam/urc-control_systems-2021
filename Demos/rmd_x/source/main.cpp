33b4f1b3fadff4aa1ab3854889cf022c21611098
coreyboy1820 <coreyboy1820@hotmail.com>
2022-03-26 12:46:49 -0700

    new rmd demo

--- Demos/rmd_x/source/main.cpp
+++ Demos/rmd_x/source/main.cpp
@@ -3,77 +3,53 @@
 #include "peripherals/lpc40xx/can.hpp"
 #include "devices/actuators/servo/rmd_x.hpp"
 #include "utility/log.hpp"
-#include "utility/build_info.hpp"
 
 int main(void)
 {
-  constexpr auto kSpeedLimit = 20_rpm;
-  constexpr auto kTimeDelay  = 1000ms;
+  constexpr auto kSpeedLimit = 5_rpm;
+  constexpr auto kTimeDelay  = 1s;
 
   sjsu::LogInfo("Starting RMD-X demo in 5s...");
-  sjsu::Can * can = nullptr;
-
-  if (sjsu::build::IsPlatform(sjsu::build::Platform::lpc40xx))
-  {
-    static sjsu::lpc40xx::Can lpc_can(sjsu::lpc40xx::Can::Channel::kCan2);
-    can = &lpc_can;
-  }
-  else if (sjsu::build::IsPlatform(sjsu::build::Platform::stm32f10x))
-  {
-    sjsu::LogInfo("CANBUS for stm32f10x not supported");
-    return -1;
-  }
-  else
-  {
-    sjsu::LogInfo("Platform not supported");
-    return -1;
-  }
-
-  sjsu::RmdX rmd_x7(*can, 0x148);
+  sjsu::lpc40xx::Can & can = sjsu::lpc40xx::GetCan<2>();
+  sjsu::StaticMemoryResource<1024> memory_resource;
+  sjsu::CanNetwork can_network(can, &memory_resource);
+  sjsu::RmdX rmd_x7(can_network, 0x141);
 
   sjsu::Delay(5s);
 
-  sjsu::LogInfo("Initializing and Enabling RMD-X driver...");
+  sjsu::LogInfo("RMD-X initializing...");
   rmd_x7.Initialize();
-  // rmd_x7.Enable();
-  // sjsu::LogInfo("RMD-X driver Intialized and Enabled!");
 
-  sjsu::LogInfo("Spinning the motor for 3s @ %f RPM...",
-                kSpeedLimit.to<double>());
+  sjsu::LogInfo("Setting motor speed to +%f RPM...", kSpeedLimit.to<double>());
   rmd_x7.SetSpeed(kSpeedLimit);
   sjsu::Delay(3s);
 
-  sjsu::LogInfo("Requesting feedback from motor and printing it...");
+  // Get status information from motor and print out the details
+  sjsu::LogInfo("Requesting feedback from motor and printing it to STDOUT...");
   rmd_x7.RequestFeedbackFromMotor().GetFeedback().Print();
 
-  sjsu::LogInfo("Setting Motor Angle Position to 0 deg...");
-  rmd_x7.SetAngle(0_deg, kSpeedLimit);
-  sjsu::LogInfo("Waiting 4s for this operation to complete...");
+  sjsu::LogInfo("Setting angle of motor to 10_deg..");
+  rmd_x7.SetAngle(10_deg, kSpeedLimit);
   sjsu::Delay(4s);
 
-  sjsu::LogInfo("Rotating motor 360 deg in 45 deg increments...");
-  for (units::angle::degree_t angle = 0_deg; angle <= 360_deg; angle += 45_deg)
-  {
-    sjsu::LogInfo(">>> %d deg angle", angle.to<int>());
-    rmd_x7.SetAngle(angle, kSpeedLimit);
-    sjsu::Delay(kTimeDelay);
-    rmd_x7.RequestFeedbackFromMotor().GetFeedback().Print();
-  }
+  // rmd_x7.SetAngle(45_deg, kSpeedLimit);
+  // sjsu::Delay(1s);
 
-  rmd_x7.RequestFeedbackFromMotor().GetFeedback().Print();
+  // rmd_x7.SetAngle(90_deg, kSpeedLimit);
+  // sjsu::Delay(1s);
 
-  sjsu::LogInfo("Rotating motor to -360 deg in -45 deg increments...");
-  for (units::angle::degree_t angle = 360_deg; angle >= -360_deg;
-       angle -= 45_deg)
-  {
-    sjsu::LogInfo(">>> %d deg angle", angle.to<int>());
-    rmd_x7.SetAngle(angle, kSpeedLimit);
-    sjsu::Delay(kTimeDelay);
-    rmd_x7.RequestFeedbackFromMotor().GetFeedback().Print();
-  }
+  // rmd_x7.SetAngle(180_deg, kSpeedLimit);
+  // sjsu::Delay(2s);
 
-  sjsu::LogInfo("Setting motor angle back to 0 degrees...");
+  // rmd_x7.SetAngle(360_deg, kSpeedLimit);
+  // sjsu::Delay(4s);
+
+  // rmd_x7.SetAngle(360_deg, kSpeedLimit);
+  // sjsu::Delay(4s);
+
+  sjsu::LogInfo("Setting motor angle back to 0 deg...");
   rmd_x7.SetAngle(0_deg, kSpeedLimit);
+  sjsu::Delay(4s);
 
   return 0;
 }
