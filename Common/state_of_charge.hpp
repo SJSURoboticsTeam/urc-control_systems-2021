16cf24727d05cc5e4cd0d600211498024d72938b
FluffyFTW <46359603+FluffyFTW@users.noreply.github.com>
2022-04-09 14:29:35 -0700

    adds the max17043 logic and demo file (#160)
    * adds the max17043 logic and demo file
    
    * implimented the state of charge thing into subsystems
    
    * changed st to state of charge

--- Common/state_of_charge.hpp
+++ Common/state_of_charge.hpp
@@ -2,52 +2,48 @@
 #include "peripherals/lpc17xx/i2c.hpp"
 #include "peripherals/lpc40xx/gpio.hpp"
 #include "peripherals/hardware_counter.hpp"
-#include "devices/sensors/battery/ltc4150.hpp"
+#include "devices/sensors/battery/ltc4150.hpp" 
 namespace sjsu::common
 {
 /// State of charge manages ltc4150 and max17043
 class StateOfCharge
 {
- public:
-  StateOfCharge() {}
-  void Initialize()
+public:
+  StateOfCharge(){}
+void Initialize(){
+  //TODO - impliment initialize for state of charge
+}
+
+double StateOfCharge_MAX(){
+  sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<2>();
+  i2c.Initialize();
+  sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 6>();
+  alert_pin.Initialize();
+  bool callback_was_called   = false;
+        InterruptCallback callback = [&callback_was_called]() {
+          callback_was_called = true;
+        };
+  uint8_t address = 0b0110110;
+
+  Max170343 *battery_sensor = new Max170343(i2c, alert_pin, callback, address);
+
+  units::voltage::volt_t battery_voltage = battery_sensor -> GetVoltage();
+  double voltage = battery_voltage.to<double>();
+
+  if(voltage < 3.05)
   {
-    // TODO - implement initialize for state of charge
+    sjsu::LogWarning("Cell Voltage below 3. Battery dangerously low!");
   }
-  double StateOfCharge_MAX()
+  else if(voltage < 3.25 )
   {
-    sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<0>();
-    i2c.Initialize();
-    sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 1>();
-    alert_pin.Initialize();
-    bool callback_was_called   = false;
-    InterruptCallback callback = [&callback_was_called]()
-    { callback_was_called = true; };
-    uint8_t address = 0b0110110;
-
-    Max170343 * battery_sensor =
-        new Max170343(i2c, alert_pin, callback, address);
-
-    units::voltage::volt_t battery_voltage = battery_sensor->GetVoltage();
-    double voltage                         = battery_voltage.to<double>();
-    return voltage / 3.3;
+    sjsu::LogWarning("Cell Voltage below 3.25. Battery is low");
   }
 
-  double StateOfCharge_LTC()
-  {
-    sjsu::lpc40xx::Gpio & gpio = sjsu::lpc40xx::GetGpio<0, 1>();
-    sjsu::GpioCounter counter(gpio, sjsu::Gpio::Edge::kRising);
+    voltage = (voltage-3)/(4.2 - 3);
 
-    sjsu::lpc40xx::Gpio & pol          = sjsu::lpc40xx::GetGpio<2, 3>();
-    units::impedance::ohm_t resistance = 10'000_Ohm;
+    voltage = voltage*100;
+  return voltage;
 
-    Ltc4150 * battery_sensor = new Ltc4150(counter, pol, resistance);
-
-    units::charge::microampere_hour_t battery_charge =
-        battery_sensor->GetCharge();
-    double charge = battery_charge.to<double>();
-    return charge / 100.0;  // 100 is a temporary and will need to be updated
-                            // when we know the battery mA/h
-  }
+}
 };
 }  // namespace sjsu::common
---
+++ Demos/max17043/source/main.cpp
@@ -0,0 +1,21 @@
+#include "utility/log.hpp"
+#include "../../Common/state_of_charge.hpp"
+
+int main(void)
+{
+  while (1)
+  {
+    try
+    {
+      sjsu::common::StateOfCharge st;
+      sjsu::Delay(1000ms);
+      sjsu::LogInfo("Max: %f%% remaining", st.StateOfCharge_MAX());
+    }
+    catch (const std::exception & e)
+    {
+      sjsu::LogError("Uncaught error in main() - Stopping Rover!");
+    }
+  }
+
+  return 0;
+}
