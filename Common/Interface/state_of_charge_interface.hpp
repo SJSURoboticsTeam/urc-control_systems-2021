aef0cef0191387d7a6d67a4629e20b99a0d8a9f2
Andrew Ingerman <andrewing777@gmail.com>
2022-04-30 10:55:58 -0700

    Stateof charge interface (#165)
    * added Get function and moved everything to Initialize
    
    * changed some floats to doubles
    
    * added two methods
    
    * split into 3 functions and fixed variables

--- Common/Interface/state_of_charge_interface.hpp
+++ Common/Interface/state_of_charge_interface.hpp
@@ -1,5 +1,13 @@
-class StateOfChargeInterface
+#pragma once
+
+namespace sjsu::common
 {
- public:
-  virtual float GetStateOfCharge();
-};
+ class StateOfChargeInterface
+  {
+  public:
+    virtual void CalcState() = 0;
+    virtual bool IsBatteryLow() = 0;
+    virtual float GetStateOfCharge() = 0;
+  };
+
+}
--- Common/state_of_charge.hpp
+++ Common/state_of_charge.hpp
@@ -10,42 +10,59 @@ class StateOfCharge
 {
  public:
   StateOfCharge() {}
+  sjsu::lpc40xx::I2c & i2c        = sjsu::lpc40xx::GetI2c<2>();
+  sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 6>();
+  InterruptCallback callback;
+  uint8_t address;
+  units::voltage::volt_t battery_voltage;
+  float voltage;
   void Initialize()
   {
     // TODO - impliment initialize for state of charge
+    i2c.Initialize();
+    alert_pin.Initialize();
+    address  = 0b0110110;
+    voltage = 0;
   }
 
-  double StateOfCharge_MAX()
+  void CalcState()
   {
-    // sjsu::lpc40xx::I2c & i2c = sjsu::lpc40xx::GetI2c<2>();
-    // i2c.Initialize();
-    // sjsu::lpc40xx::Gpio & alert_pin = sjsu::lpc40xx::GetGpio<0, 6>();
-    // alert_pin.Initialize();
-    // bool callback_was_called   = false;
-    // InterruptCallback callback = [&callback_was_called]()
-    // { callback_was_called = true; };
-    // uint8_t address = 0b0110110;
+    uint8_t address = 0b0110110;
+    Max170343 * battery_sensor =
+        new Max170343(i2c, alert_pin, callback, address);
+    units::voltage::volt_t battery_voltage = battery_sensor->GetVoltage();
+    double voltage                         = battery_voltage.to<double>();
+    voltage = (voltage - 3) / (4.2 - 3);
+    voltage = voltage * 100;
+  }
 
-    // Max170343 * battery_sensor =
-    //     new Max170343(i2c, alert_pin, callback, address);
+  bool IsBatteryLow()
+  {
+    bool callback_was_called = true;
+    if (voltage < 3.05)
+    {
+      sjsu::LogWarning("Cell Voltage below 3. Battery dangerously low!");
+      return true;
+    }
+    else if (voltage < 3.25)
+      InterruptCallback callback = [&callback_was_called]()
+      { callback_was_called = true; };
+     
+    if (voltage < 3.05)
+    {
+      sjsu::LogWarning("Cell Voltage below 3. Battery dangerously low!");
+      return true;
+    }
+    else if (voltage < 3.25)
+    {
+      sjsu::LogWarning("Cell Voltage below 3.25. Battery is low");
+    }
+     return false;
+  }
 
-    // units::voltage::volt_t battery_voltage = battery_sensor->GetVoltage();
-    // double voltage                         = battery_voltage.to<double>();
-
-    // if (voltage < 3.05)
-    // {
-    //   sjsu::LogWarning("Cell Voltage below 3. Battery dangerously low!");
-    // }
-    // else if (voltage < 3.25)
-    // {
-    //   sjsu::LogWarning("Cell Voltage below 3.25. Battery is low");
-    // }
-
-    // voltage = (voltage - 3) / (4.2 - 3);
-
-    // voltage = voltage * 100;
-    // return voltage;
-    return 0;
+  float GetStateOfCharge()
+  {
+    return voltage;
   }
 };
 }  // namespace sjsu::common
